eac336d17721143c235f59274c0c46e7c3584a28
kauan09112007 <145721260+kauan09112007@users.noreply.github.com>
2023-09-28 20:04:27 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,5 @@
+function somar(){
+  var valor1 = parseInt(document.gatElementById("nun1").value, 10);
+  var valor2 = parseInt(document.gatElementById("nun2").value, 10);
+  document.gatElementById("resultado").value=valor1+valor2;
+}
